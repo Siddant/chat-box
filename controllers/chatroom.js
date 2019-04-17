76550f3f8ea49b5c273319f420eeef864a49bdff
Siddant <siddantgurung@hotmail.co.uk>
2019-04-17 14:11:20 +0100

    trying to find and create solution

--- controllers/chatroom.js
+++ controllers/chatroom.js
@@ -1,25 +1,29 @@
 const Chatroom = require('../models/chatroom')
 
 function createRoute(req, res) {
-    // console.log(req.body)
-    // res.status(201).json({ message: req.body.user })
-    // res.status(201).json({ message: 'message has been created sucessfully' })
-    Chatroom
-        .create(req.body)
-        .then(() => res.status(201).json({ message: 'message has been created sucessfully' }))
-        .catch(err => res.status(201).json({ errors: err }))
+    res.status(201).json({ message: req.body.message })
+
+    // Chatroom
+    //     .create(req.body)
+    //     .then(() => res.status(201).json({ message: 'message has been created sucessfully' }))
+    //     .catch(err => res.status(201).json({ errors: err }))
 }
 
 function showRoute(req, res) {
-    const arr = ['5cb51949c94e70535a38a039', '5cb51dc4452adb56b8127eeb']
+    const arr = ['5cb51949c94e70535a38a039', '5cb5174249414f4e9dec2709']
     Chatroom
-        .find({ 'user1': { $in: arr }, 'user2': { $in: arr } })
+        .find({ 'user': { $all: arr } })
         // .populate('user1')
         // .populate('user2')
         .then((messages) => res.status(201).json(messages))
         .catch(err => res.status(201).json({ errors: err }))
 }
 
+
+// if (messages.length > 0) {
+//     res.status(201).json(messages)
+
+// }
 
 module.exports = {
     create: createRoute,
