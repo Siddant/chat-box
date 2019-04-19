5c706c483478de23af2ec47af9298e01ec7242f8
Siddant <siddantgurung@hotmail.co.uk>
2019-04-19 22:17:09 +0100

    back-end works

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,36 +1,8 @@
-// import todoRoutes from './chatroom';
-// import todoController from './chatroom';
 const todoController = require('./chatroom')
-
-// require('socket.io')(server)
-
 function respondRoute(socket) {
-
-
-    // socket.on('chat message', function (msg) {
-    //     console.log('message: ' + msg);
-    //     io.emit('chat message', msg.message)
-
-    // });
-
-
     socket.on('chat message', function (msg) {
-        // global.tesio.emit('chat message', msg.message)
         todoController.testing(msg)
-
     })
-    // io.on('chat message', function (msg) {
-    //     // console.log(msg.message)
-    //     // todoController.testing(io, msg)
-    //     // io.emit('chat message', msg.message)
-    //     io.emit('chat message', msg.message)
-
-    //     // socket.on('chat message', function (msg) {
-    //     //     console.log('message: ' + msg);
-    //     //     io.emit('chat message', msg.message)
-
-    //     // });
-    // });
 }
 
 
